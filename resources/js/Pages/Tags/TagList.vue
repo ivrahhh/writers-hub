c296578e1cb47b0839913ee4940a8906ee247305
harvicapino22 <harvicapino22@gmail.com>
2022-12-15 09:51:30 +0800

    small fix

--- resources/js/Pages/Tags/TagList.vue
+++ resources/js/Pages/Tags/TagList.vue
@@ -33,36 +33,42 @@ const remove = (event) => {
             <i class="fa-solid fa-plus"></i>
             Add new Tag
         </Link>
-        <div class="relative overflow-x-auto bg-white shadow rounded-lg ring-1 ring-black ring-opacity-5">
-            <table class="text-sm text-left w-full">
-                <thead>
-                    <tr class="select-none bg-slate-900 text-white border-b font-semibold">
-                        <td scope="col" class="px-6 py-3">Tag ID</td>
-                        <td scope="col" class="px-6 py-3">Tag</td>
-                        <td scope="col" class="px-6 py-3">Added at</td>
-                        <td class="sr-only">Action</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="tag in tags.data" :key="tag.id" class="odd:bg-white even:bg-gray-50">
-                        <td class="px-6 py-3">{{ tag.id }}</td>
-                        <td class="px-6 py-3">{{ tag.tag }}</td>
-                        <td class="px-6 py-3">{{ new Date(tag.created_at).toLocaleDateString() }}</td>
-                        <td class="px-6 py-3">
-                            <Link @click.prevent="remove" :href="route('tags.destroy', {tag: tag.id})" class="text-sm hover:text-red-600">
-                                <i class="fa-solid fa-trash mr-2"></i>Remove
-                            </Link> 
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div v-if="tags.total">
+            <div class="relative overflow-x-auto bg-white shadow rounded-lg ring-1 ring-black ring-opacity-5">
+                <table class="text-sm text-left w-full">
+                    <thead>
+                        <tr class="select-none bg-slate-900 text-white border-b font-semibold">
+                            <td scope="col" class="px-6 py-3">Tag</td>
+                            <td scope="col" class="px-6 py-3">Added at</td>
+                            <td class="sr-only">Action</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="tag in tags.data" :key="tag.id" class="odd:bg-white even:bg-gray-50">
+                            <td class="px-6 py-3">{{ tag.tag }}</td>
+                            <td class="px-6 py-3">{{ new Date(tag.created_at).toLocaleDateString() }}</td>
+                            <td class="px-6 py-3">
+                                <Link @click.prevent="remove" :href="route('tags.destroy', {tag: tag.id})" class="text-sm hover:text-red-600">
+                                    <i class="fa-solid fa-trash mr-2"></i>Remove
+                                </Link> 
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <Pagination 
+                :links="tags.links" 
+                :from="tags.from"
+                :to="tags.to"
+                :total="tags.total"
+            />
         </div>
-        <Pagination 
-            :links="tags.links" 
-            :from="tags.from"
-            :to="tags.to"
-            :total="tags.total"
-        />
+        <div class="block w-full rounded-lg bg-gray-200 h-56" v-else>
+            <div class="flex flex-col gap-4 items-center justify-center h-full text-gray-500">
+                <i class="fa-solid fa-tags text-4xl"></i>
+                <span class="text-xl font-semibold">No Tags Created</span>
+            </div>
+        </div>
     </AdminLayout>
     <Toast :is-active="active" title="Tag Removed" message="Tag has been removed successfully" type="success" />
 </template>
