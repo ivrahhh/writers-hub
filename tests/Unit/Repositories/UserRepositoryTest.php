1ce030be9c464cd7f6526b58d6374e30539cf093
harvicapino22 <harvicapino22@gmail.com>
2022-12-20 10:09:31 +0800

    added new test

---
+++ tests/Unit/Repositories/UserRepositoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit\Repositories;
+
+use App\Models\User;
+use App\Repositories\UserRepository;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserRepositoryTest extends TestCase
+{
+    public function testCanCreateNewUser()
+    {
+        $userRepository = new UserRepository;
+
+        $user = $userRepository->create([
+            'email' => fake()->email,
+            'password' => Hash::make('password'),
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => $user->email, 
+        ]);
+    }
+
+    public function testItWillReturnAUserModel()
+    {
+        $userRepository = new UserRepository;
+
+        $user = $userRepository->create([
+            'email' => fake()->email(),
+            'password' => Hash::make('password'),
+        ]);
+
+        $this->assertTrue($user instanceof User);
+    }
+
+    public function testItWillThrowQueryExceptionIfEmailIsAlreadyUsed()
+    {
+        $hell = User::factory()->create();
+
+        $this->assertThrows(function() use ($hell) {
+            $userRepository = new UserRepository;
+            $userRepository->create(['email' => $hell->email, 'password' => Hash::make('password')]);
+        }, QueryException::class);
+    }
+}
